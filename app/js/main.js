b87a96839881fbe029bdb87dbf3cdf7d199b5f3b
alexandr05shev4enko <alex050798@gmail.com>
2021-01-07 12:42:26 +0200

    added contacts

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,28 @@
-$(function(){
+$(function () {
 
   $(".header-top__menu a").on("click", function (event) {
-		event.preventDefault();
-		//забираем идентификатор блока с атрибута href
-		var id  = $(this).attr('href'),
-		//узнаем высоту от начала страницы до блока на который ссылается якорь
-			top = $(id).offset().top;
-		//анимируем переход на расстояние - top за 1500 мс
-		$('body,html').animate({scrollTop: top}, 1500);
+    event.preventDefault();
+    //забираем идентификатор блока с атрибута href
+    var id = $(this).attr('href'),
+      //узнаем высоту от начала страницы до блока на который ссылается якорь
+      top = $(id).offset().top;
+    //анимируем переход на расстояние - top за 1500 мс
+    $('body,html').animate({ scrollTop: top }, 1500);
   });
 
+  $(function () {
+
+    $(window).scroll(function () {
+      if ($(this).scrollTop() != 0) {
+        $('#toTop').fadeIn();
+      } else {
+        $('#toTop').fadeOut();
+      }
+    });
+    $('#toTop').click(function () {
+      $('body,html').animate({ scrollTop: 0 }, 800);
+    });
+  });
 
 
 });
